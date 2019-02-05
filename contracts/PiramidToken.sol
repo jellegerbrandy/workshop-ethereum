5bfbad8d47e1c683a631b2b0afe43ecec12f3ac9
geckoslair <enrico@piramid.studio>
2019-02-05 10:10:58 +0100

    test basic sanity

--- contracts/PiramidToken.sol
+++ contracts/PiramidToken.sol
@@ -7,7 +7,7 @@ contract PiramidToken is ERC20, ERC20Detailed {
 
 
     uint8 public constant DECIMALS = 18;
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
+    uint256 public constant INITIAL_SUPPLY = 21000000 * (10 ** uint256(DECIMALS));
 
     constructor () public ERC20Detailed("PiramidToken", "PRMD", DECIMALS) {
         _mint(msg.sender, INITIAL_SUPPLY);
---
+++ test/piramidToken.js
@@ -0,0 +1,12 @@
+var PiramidToken = artifacts.require('./PiramidToken.sol')
+
+contract('PiramidToken', function (accounts) {
+  it('test basic sanity', async function () {
+    let token = await PiramidToken.new()
+    let prmd = Math.pow(10, 18)
+    // accounts[0] is the contract owner
+    let balance = await token.balanceOf(accounts[0])
+    // we expect the initial balanace of the owner to be 21M * 18 point precision
+    assert.equal(balance.valueOf(), 21000000 * prmd)
+  })
+})
